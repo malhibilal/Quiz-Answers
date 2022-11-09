b4c310191b808b1978bdab97fe36dfe183c5caff
malhibilal <110643036+malhibilal@users.noreply.github.com>
2022-11-09 14:20:41 +0100

    Create solutions.java
    A simple code for printing name and city

---
+++ solutions.java
@@ -0,0 +1,10 @@
+public class Assignment{
+  public String  name= "Bilal";
+  public String city = "Simrishamn";
+  
+  Public static void main (String [] args{
+    System .out.println("my name is "+ name+ "and i live in "+city);
+                        }
+                      }
+                        
+  
